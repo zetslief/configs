253b0ac2610bdc41cc57a51928c042331fe22055
Konstantin Senekvich <konstantin.senkevich@atomicus.de>
2021-10-10 13:41:12 +0300

    Installation script.

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo apt install dmenu xmobar xmonad fish stalonetray nm-applet classicmenu-indicator pasystray
+
+cp -r nvim $HOME/.config/
+cp -r xmobar $HOME/.config/
+cp -r fish $HOME/.config/
+
