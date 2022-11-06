6e917e916751f09ea575161e9a7ca3e934f9850c
Konstantin Konstantin <zetslief@protonmail.com>
2022-11-07 00:31:00 +0300

    Neovim lua config.

---
+++ .xmonad/huion.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Left Top button
+xsetwacom set "HUION Huion Tablet Pad pad" "Button" "1" "pan"
+
+# Left Bottom button
+xsetwacom set "HUION Huion Tablet Pad pad" "Button" "2" "key +ctrl +z"
+
+# Right Top button
+xsetwacom set "HUION Huion Tablet Pad pad" "Button" "3" "button +3"
+
+# Right Bottom button
+xsetwacom set "HUION Huion Tablet Pad pad" "Button" "8" "button +3"
--- collect_current_config.sh
+++ collect_current_config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cp $HOME/.config/nvim/init.vim nvim/init.vim
+cp $HOME/.config/nvim/init.lua nvim/init.lua
 cp $HOME/.config/xmobar/xmobar.config xmobar/xmobar.config
 cp $HOME/.xmonad/*.hs .xmonad/
 cp $HOME/.xmonad/xmonad-session-rc .xmonad/
