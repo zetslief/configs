97c0ec05a72e6d91b906721163de249946dffbf9
Konstantin Konstantin <zetslief@protonmail.com>
2022-11-08 00:30:48 +0300

    Add nvim-tree as dependency.

--- nvim/init.lua
+++ nvim/init.lua
@@ -17,7 +17,13 @@ require "paq" {
 	"junegunn/fzf.vim";
 
 	"nvim-treesitter/nvim-treesitter";
+
+    "nvim-tree/nvim-web-devicons";
+    "nvim-tree/nvim-tree.lua";
 }
+
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
 
 vim.opt.termguicolors = true
 vim.o.colorcolumn = '80'
@@ -145,3 +151,21 @@ require('nvim-treesitter.configs').setup {
 require'lspconfig'.glslls.setup {
     { "glsl", "vert", "frag" }
 }
+
+require("nvim-tree").setup({
+  sort_by = "case_sensitive",
+  view = {
+    adaptive_size = true,
+    mappings = {
+      list = {
+        { key = "u", action = "dir_up" },
+      },
+    },
+  },
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = true,
+  },
+})
